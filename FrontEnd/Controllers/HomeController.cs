55bb5b43ac9f10e86a2d311d35590d093337caf9
Andres Zuñiga Umaña <andrezu1998@hotmail.es>
2018-08-09 10:39:52 -0600

    Revert "Merge branch 'master'"
    This reverts commit 8c018c736366731d64f8dca97c66ba16b1b407b2, reversing
    changes made to 103e999f17a1b904b784b0b468acd5f1916eb67e.

--- FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace FrontEnd.Controllers
 {
-    
+    [Authorize(Roles = "Administrador, Consulta")]
     public class HomeController : Controller
     {
         public ActionResult Index()
--- UnitTests/pruebasUsuario.cs
+++ UnitTests/pruebasUsuario.cs
@@ -47,11 +47,11 @@ namespace UnitTests
             List<T_personas> usuarios = new List<T_personas>();
             IPersonaBLL usuarioBLL = new PersonaBLLImpl();
 
-            usuarios = usuarioBLL.buscarPorNombre("11");
+            usuarios = usuarioBLL.buscarPorNombre("Andres");
 
             foreach (T_personas usuario in usuarios)
             {
-                if (usuario.apellido1 == "11")
+                if (usuario.apellido1 == "Zuñiga")
                 {
                     usuarioBLL.Remove(usuario);
                 }
