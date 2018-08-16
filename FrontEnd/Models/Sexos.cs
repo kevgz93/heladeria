240e412a8725aa4dee392ec070fcbb06bb83ef39
kevgz93 <kguerreroz93@gmail.com>
2018-08-16 14:39:45 -0700

    change to merge

---
+++ FrontEnd/Models/Sexos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models
+{
+    public class Sexos
+    {
+
+        public string nombre { get; set; }
+
+        public string codigo { get; set; }
+    }
+}
